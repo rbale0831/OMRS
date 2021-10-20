39e2b17d7f31ca5f48b8d7043d0fe3b9d349cf17
saleem2250 <Saleem2250@eng.rizvi.edu.in>
2021-10-20 19:43:30 +0530

    added registeration page and some minor changes in login page

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const hospitalRoutes = require('./routes/hospitalRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const cookieParser = require('cookie-parser');
 const { userAuth } = require('./middleware/authMiddleware');
+const path = require('path')
+// const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -14,20 +16,27 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/public', express.static('/public'))
 
 
 // register view engine
 app.set('view engine', 'ejs');
-const dbURL = 'mongodb+srv://rbale0831:rohit3108@cluster0.dzwxs.mongodb.net/partial';
+const dbURL = 'mongodb+srv://rbale0831:rohit3108@cluster0.dzwxs.mongodb.net/kim';
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true/*, useCreateIndex: true*/})
     .then((result) => app.listen(9000))
     .catch((error) => console.log(error));
 
-
+const static_path=path.join(__dirname,"./views");
+// console.log(path.join(__dirname,"./public"));
+app.use(express.static(static_path))
 // Normal Routes
 app.get('/', (req, res) => {
     res.redirect('/home');
 });
+
+app.get('/register',(req,res)=>{
+    res.render("./user/index")
+})
 
 // info_page Routes
 app.use('/home', infoPageRoutes); 
