e7dbbceea218d30e69054566ac0dc08b9208bd5c
Saleem2001 <sksaleem8097@gmail.com>
2022-01-24 12:52:04 +0530

    design-signup page

--- views/infoPage/js/signup.js
+++ views/infoPage/js/signup.js
@@ -1,42 +1,41 @@
-const form = document.querySelector('form');
-    const unameError = document.querySelector('.uname.error');
-    const emailError = document.querySelector('.email.error');
-    const passwordError = document.querySelector('.password.error');
+const form = document.querySelector("form");
+const unameError = document.querySelector(".uname.error");
+const emailError = document.querySelector(".email.error");
+const passwordError = document.querySelector(".password.error");
 
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault();
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-      // reset errors
-      unameError.textContent = '';
-      emailError.textContent = '';
-      passwordError.textContent = '';
+  // reset errors
+  unameError.textContent = "";
+  emailError.textContent = "";
+  passwordError.textContent = "";
 
-      // get values
-      const fname = form.fname.value;
-      const lname = form.lname.value;
-      const uname = form.uname.value;
-      const email = form.email.value;
-      const password = form.password.value;
-      const password = form.password.value;
+  // get values
+  const fname = form.fname.value;
+  const lname = form.lname.value;
+  const uname = form.uname.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  const password = form.password.value;
 
-      try {
-        const res = await fetch('/home/signup', { 
-          method: 'POST', 
-          body: JSON.stringify({ fname, lname, uname, email, password }),
-          headers: {'Content-Type': 'application/json'}
-        });
-        const data = await res.json();
-        console.log(data);
-        if (data.errors) {
-          unameError.textContent = data.errors.uname;
-          emailError.textContent = data.errors.email;
-          passwordError.textContent = data.errors.password;
-        }
-        if (data.user) {
-          location.assign('/home/login');
-        }
-      }
-      catch (err) {
-        console.log(err);
-      }
+  try {
+    const res = await fetch("/home/signup", {
+      method: "POST",
+      body: JSON.stringify({ fname, lname, uname, email, password }),
+      headers: { "Content-Type": "application/json" },
     });
+    const data = await res.json();
+    console.log(data);
+    if (data.errors) {
+      unameError.textContent = data.errors.uname;
+      emailError.textContent = data.errors.email;
+      passwordError.textContent = data.errors.password;
+    }
+    if (data.user) {
+      location.assign("/home/login");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
