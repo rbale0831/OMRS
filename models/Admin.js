ef410d53e6a3a0eac858d7b243a2978d9fc5d28f
rbale0831 <70151299+rbale0831@users.noreply.github.com>
2022-01-24 12:04:21 +0530

    22 edit

--- models/Admin.js
+++ models/Admin.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
  
 const adminSchema = new mongoose.Schema({
     fname:{
