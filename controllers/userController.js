9577fa4a6a6b73412f03b73639902c33d615e306
rbale3108 <91748984+rbale3108@users.noreply.github.com>
2022-01-30 15:45:52 +0530

    controller edit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,32 +2,19 @@ const User = require('../models/User');
 
 
 module.exports.userDashboard_get = (req, res)=>{
-    res.status(200).render('user/index');
-};
-module.exports.userDashboard_post = async (req, res)=>{
-    const { fname, lname, uname, email, password } = req.body;
-    try {
-        const user = await User.create({ fname, lname, uname, email, password });
-        const token = createToken(user._id);
-        res.cookie('csign', token, { httpOnly: true, maxAge: maxAge * 3 });
-        res.status(201).json({ user: user._id });
-    }
-    catch(err){
-        const errors = handleErrors(err);
-        res.status(400).json({ errors });
-    }
+    res.status(200).render('user/index', { title: 'Profile' });
 };
 module.exports.userAppointment_History_get = (req, res)=>{
-    res.status(200).render('user/Appointment_History');
+    res.status(200).render('user/Appointment_History', { title: 'Appointment History' });
 };
 module.exports.userBook_Appointment_get = (req, res)=>{
-    res.status(200).render('user/Book_Appointment');
+    res.status(200).render('user/Book_Appointment', { title: 'Book Appointment' });
 };
 module.exports.userRecords_get = (req, res)=>{
-    res.status(200).render('user/Records');
+    res.status(200).render('user/Records', { title: 'Records' });
 };
 module.exports.userProfile_get = (req, res)=>{
-    res.status(200).render('user/profile');
+    res.status(200).render('user/profile', { title: '' });
 };
 module.exports.userLogout_get = (req, res) => {          
     res.cookie('clogin','',{ maxAge: 1 });
